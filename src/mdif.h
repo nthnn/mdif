52663bc26459a253668249011c9ec0e1f6c0ce69
Nathanne Isip <nathanneisip@gmail.com>
2024-07-07 13:05:03 +0800

    MDIF I/O errors on header file.

--- src/mdif.h
+++ src/mdif.h
@@ -29,12 +29,28 @@ typedef struct mdif_struct {
     unsigned char *alpha;
 } mdif_t;
 
+typedef enum mdif_error {
+    MDIF_ERROR_IO,
+    MDIF_ERROR_NONE,
+    MDIF_ERROR_INVALID_SIGNATURE,
+    MDIF_ERROR_CANNOT_ALLOCATE,
+    MDIF_ERROR_INVALID_WIDTH,
+    MDIF_ERROR_INVALID_HEIGHT,
+    MDIF_ERROR_READ,
+    MDIF_ERROR_WRITE,
+    MDIF_ERROR_INVALID_FILE_HANDLE,
+    MDIF_ERROR_IMAGE,
+    MDIF_ERROR_GRAYSCALE
+} mdif_error_t;
+
 void mdif_init(mdif_t* image, short width, short height);
 void mdif_free(mdif_t* image);
 
-int mdif_read(const char* filename, mdif_t* image);
-int mdif_write(const char* filename, mdif_t* image);
+mdif_error_t mdif_read(const char* filename, mdif_t* image);
+mdif_error_t mdif_write(const char* filename, mdif_t* image);
 
-int mdif_grayscale(mdif_t* image, float* grayscale);
+mdif_error_t mdif_grayscale(mdif_t* image, float* grayscale);
+
+const char* mdif_error_message(mdif_error_t error_num);
 
 #endif
