c1b1c969c2b1bc5e121ee4a7adfedb6211d23cb0
Nathanne Isip <nathanneisip@gmail.com>
2024-07-07 17:58:53 +0800

    Fixed generate gradient build script.

--- examples/generate_gradient/build.bat
+++ examples/generate_gradient/build.bat
@@ -1,1 +1,1 @@
-gcc -o ..\..\dist\generate_gradient.exe -I..\..\src ..\..\src\mdif.cpp generate_gradient.cpp
+g++ -x c++ -o ..\..\dist\generate_gradient.exe -I..\..\src ..\..\src\mdif.cpp generate_gradient.ino
--- examples/generate_gradient/build.sh
+++ examples/generate_gradient/build.sh
@@ -1,1 +1,1 @@
-gcc -o ../../dist/generate_gradient -I../../src ../../src/mdif.cpp generate_gradient.cpp
+g++ -x c++ -o ../../dist/generate_gradient -I../../src ../../src/mdif.cpp generate_gradient.ino
